4b51e7b2d1847e2309341b0305c8af17b02f8d48
HEMANTHKUMAR MN <95921220+HemanthdkumarMN-devOps@users.noreply.github.com>
2022-11-29 18:05:54 +0530

    Update App.java

--- test/src/main/java/com/example/tests/App.java
+++ test/src/main/java/com/example/tests/App.java
@@ -15,7 +15,9 @@ public class App {
 	@BeforeClass(alwaysRun = true)
 	public void setUp() throws Exception {
 		System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-		
+		ChromeOptions options = new ChromeOptions()
+                options.addArgument("headless");
+                ChromeDriver driver = new ChromeDriver(options);
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
