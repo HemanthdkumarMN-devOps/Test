07839995dbaf0b3bb2a833955673e8a4dbd2983c
HEMANTHKUMAR MN <95921220+HemanthdkumarMN-devOps@users.noreply.github.com>
2022-11-29 18:20:50 +0530

    Update App.java

--- test/src/main/java/com/example/tests/App.java
+++ test/src/main/java/com/example/tests/App.java
@@ -7,43 +7,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 @Test
-
 public class App {
-	private WebDriver driver;
-	@BeforeClass(alwaysRun = true)
-	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver(options);
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	}
+    private WebDriver driver;
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver",
+                "/usr/bin/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
 
+    @Test(priority = 1)
+    public void login() throws Exception {
+          driver.get("https://immutly-dev.netlify.app/");
+            driver.findElement(By.id("login-button")).click();
+            driver.findElement(By.id("username")).clear();
+            driver.findElement(By.id("username")).sendKeys("cocseth9@gmail.com");
+            driver.findElement(By.id("password")).click();
+            driver.findElement(By.id("password")).clear();
+            driver.findElement(By.id("password")).sendKeys("Test@123");
+            driver.findElement(By.name("action")).click();
+    
+    }
+    
+    @Test(priority = 2)
+    public void buyNFT() {
+        
+         driver.findElement(By.xpath("/html/body/div/div/div[3]/div[2]/div/div/div[1]/div/div/div[1]/img")).click();
+            driver.findElement(By.id("nftpage-buynow")).click();
+            driver.findElement(By.id("buynftmodal-confirm")).click();
+            
+    }
 
-
-	@Test(priority = 1)
-	public void login() throws Exception {
-		  driver.get("https://immutly-dev.netlify.app/");
-		    driver.findElement(By.id("login-button")).click();
-		    driver.findElement(By.id("username")).clear();
-		    driver.findElement(By.id("username")).sendKeys("cocseth9@gmail.com");
-		    driver.findElement(By.id("password")).click();
-		    driver.findElement(By.id("password")).clear();
-		    driver.findElement(By.id("password")).sendKeys("Test@123");
-		    driver.findElement(By.name("action")).click();
-	
-	}
-	
-	@Test(priority = 2)
-	public void buyNFT() {
-		
-		 driver.findElement(By.xpath("/html/body/div/div/div[3]/div[2]/div/div/div[1]/div/div/div[1]/img")).click();
-		    driver.findElement(By.id("nftpage-buynow")).click();
-		    driver.findElement(By.id("buynftmodal-confirm")).click();
-		    
-	}
-
-	
-		
-	}
+    
+        
+    }
