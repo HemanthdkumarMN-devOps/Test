4313e01df6748cf059159ce27065cc9f9aa4f324
HEMANTHKUMAR MN <95921220+HemanthdkumarMN-devOps@users.noreply.github.com>
2022-11-30 10:23:38 +0530

    Update App.java

--- test/src/main/java/com/example/tests/App.java
+++ test/src/main/java/com/example/tests/App.java
@@ -19,14 +19,8 @@ public class App {
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("https://google.com");
+        driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         
     }
 
