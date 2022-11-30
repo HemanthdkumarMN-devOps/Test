d437363de601b7f389f26879a4397b2b6e5cf1a1
HEMANTHKUMAR MN <95921220+HemanthdkumarMN-devOps@users.noreply.github.com>
2022-11-30 11:42:31 +0530

    Update App.java

--- test/src/main/java/com/example/tests/App.java
+++ test/src/main/java/com/example/tests/App.java
@@ -21,6 +21,7 @@ public class App {
        
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
+        WebDriver driver = new ChromeDriver() ;
         options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
