ffcc79c33cec0b283567af6e919a2475ee2861f4
HEMANTHKUMAR MN <95921220+HemanthdkumarMN-devOps@users.noreply.github.com>
2022-12-12 12:34:16 +0530

    Update App.java

--- test/src/main/java/com/example/tests/App.java
+++ test/src/main/java/com/example/tests/App.java
@@ -1,24 +1,17 @@
 package com.example.tests;
-
-
-
+ 
 import java.time.Duration;
-
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
-
-
-
+ 
 @Test(priority = 1)
 public class App {
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver",
-           "/usr/local/bin/chromedriver");     
+           "/usr/local/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--window-size=1920x1080");
@@ -26,25 +19,35 @@ public class App {
         options.addArguments("--disable-gpu");
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-
-
-       driver.get("https://immutly-dev.netlify.app/");
+ 
+        driver.get("https://immutly.ml/");
         driver.findElement(By.id("login-button")).click();
         driver.findElement(By.id("username")).clear();
-        driver.findElement(By.id("username")).sendKeys("cocseth9@gmail.com");
+        driver.findElement(By.id("username")).sendKeys("techbone702@gmail.com");
         driver.findElement(By.id("password")).click();
         driver.findElement(By.id("password")).clear();
-        driver.findElement(By.id("password")).sendKeys("Test@123");
+        driver.findElement(By.id("password")).sendKeys("test@123");
         driver.findElement(By.name("action")).click();
-
-
-
-   }
-
-
+ 
+        // BuyNFT
+        driver.findElement(By.xpath("/html/body/div/div/div/div[3]/div[2]/div[2]/div[1]/div/div/div[1]/img")).click();
+        driver.findElement(By.id("nftpage-buynow")).click();
+        driver.findElement(By.id("buynftmodal-confirm")).click();
+        
+        //list
+        driver.findElement(By.xpath("/html/body/div/div/div/div[3]/div[5]/div/div/table/tbody/tr[1]/td[5]/button")).click();
+        driver.findElement(By.id("nftpage-list-nft")).click();
+        driver.findElement(By.xpath("//div[@id='root']/div/div/div[3]/div/div/div/div/div/div[2]/div[2]/span[2]"))
+                .click();
+        driver.findElement(By.id("list-public-price")).click();
+        driver.findElement(By.id("list-public-price")).click();
+        driver.findElement(By.id("list-public-price")).clear();
+        driver.findElement(By.id("list-public-price")).sendKeys("1000");
+        driver.findElement(By.id("listnftmodal-submit")).click();
+ 
+    }
+ 
 }
-
 
 
 
